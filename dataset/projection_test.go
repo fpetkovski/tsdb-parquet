f852da48c8bced5fe404c8e11a194cef627cc013
Filip Petkovski <filip.petkovsky@gmail.com>
2023-06-25 08:22:39 +0200

    Add more test cases

--- dataset/projection_test.go
+++ dataset/projection_test.go
@@ -14,9 +14,59 @@ func TestProjectColumns(t *testing.T) {
 		rows      [][]testRow
 		selection []pickRange
 
-		project  []string
-		expected [][][]parquet.Value
+		columns   []string
+		chunkSize int64
+		expected  [][][]parquet.Value
 	}{
+		{
+			name: "single column projection",
+			rows: [][]testRow{{
+				twoColumnRow("val1", "val1"),
+				twoColumnRow("val1", "val2"),
+				twoColumnRow("val1", "val3"),
+				twoColumnRow("val1", "val4"),
+			}, {
+				twoColumnRow("val2", "val5"),
+				twoColumnRow("val1", "val6"),
+				twoColumnRow("val2", "val7"),
+				twoColumnRow("val2", "val8"),
+			}},
+			columns:   []string{"ColumnB"},
+			chunkSize: 3,
+			selection: []pickRange{pick(2, 6)},
+
+			expected: [][][]parquet.Value{{
+				{pqVal("val3", 1), pqVal("val4", 1), pqVal("val5", 1)},
+			}, {
+				{pqVal("val6", 1)},
+			}},
+		},
+		{
+			name: "disjoint selection",
+			rows: [][]testRow{{
+				twoColumnRow("val1", "val1"),
+				twoColumnRow("val1", "val2"),
+				twoColumnRow("val1", "val3"),
+				twoColumnRow("val1", "val4"),
+			}, {
+				twoColumnRow("val2", "val5"),
+				twoColumnRow("val1", "val6"),
+				twoColumnRow("val2", "val7"),
+				twoColumnRow("val2", "val8"),
+				twoColumnRow("val2", "val9"),
+			}},
+			columns:   []string{"ColumnB"},
+			chunkSize: 3,
+			selection: []pickRange{pick(0, 2), pick(3, 5), pick(6, 9)},
+
+			expected: [][][]parquet.Value{{
+				{pqVal("val1", 1), pqVal("val2", 1), pqVal("val4", 1)},
+			}, {
+				{pqVal("val5", 1), pqVal("val7", 1), pqVal("val8", 1)},
+			}, {
+				{pqVal("val9", 1)},
+			}},
+		},
 		{
 			name: "two column projection",
 			rows: [][]testRow{{
@@ -30,7 +80,8 @@ func TestProjectColumns(t *testing.T) {
 				twoColumnRow("val2", "val7"),
 				twoColumnRow("val2", "val8"),
 			}},
-			project:   []string{"ColumnA", "ColumnB"},
+			columns:   []string{"ColumnA", "ColumnB"},
+			chunkSize: 2,
 			selection: []pickRange{pick(2, 6)},
 
 			expected: [][][]parquet.Value{{
@@ -52,7 +103,7 @@ func TestProjectColumns(t *testing.T) {
 				rowGroup: file.RowGroups()[0],
 				ranges:   tcase.selection,
 			}
-			projections := ProjectColumns(selection, &nopSectionLoader{}, 2, "ColumnA", "ColumnB")
+			projections := ProjectColumns(selection, &nopSectionLoader{}, tcase.chunkSize, tcase.columns...)
 			defer projections.Close()
 			for {
 				values, err := projections.NextBatch()
